604976d4ec3d777f226c4e2c6c7f3ff61b0816ed
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2025-09-14 22:00:15 +0200

    New Hash

--- library/src/commonTest/kotlin/org/angproj/sec/rand/HashImpl.kt
+++ library/src/commonTest/kotlin/org/angproj/sec/rand/HashImpl.kt
@@ -14,16 +14,60 @@
  */
 package org.angproj.sec.rand
 
+import org.angproj.sec.util.TypeSize
 
-interface Hash : Sponge {
-     fun digest(): ByteArray {
-        val digestBytes = ByteArray(visibleSize * 8)
+
+abstract class Hash<E: Sponge>(private val sponge: E) {
+
+    var remainder = byteArrayOf()
+    var position = 0
+
+    /**
+     * Absorbs the input ByteArray into the sponge state, treating the input as big endian.
+     * On a little endian system, bytes are reversed before conversion to Long.
+     */
+    fun update(input: ByteArray) {
+        val data = remainder + input
+        var chunks = data.size.floorDiv(TypeSize.longSize)
+        var end = 0
+        var start = 0
+        repeat(chunks) { index ->
+            end = index * TypeSize.longSize
+            start = end + TypeSize.longSize
+            var value: Long = 0
+            (start-1 downTo end).forEach { byte ->
+                value = (value shl 8) or (byte.toLong() and 0xff)
+            }
+            sponge.absorb(value, position++ % sponge.visibleSize)
+            if(position == sponge.visibleSize) {
+                position = 0
+                sponge.round()
+            }
+        }
+        remainder = data.copyOfRange(start, data.size)
+    }
+
+    /**
+     * Returns the digest as a ByteArray in big endian order.
+     * Converts each internal little endian Long to big endian bytes.
+     */
+    fun digest(): ByteArray {
+        if(remainder.isNotEmpty()) {
+            update(ByteArray(TypeSize.longSize - remainder.size % TypeSize.longSize))
+        }
+        if(position != 0) {
+            sponge.round()
+        }
+        sponge.scramble()
+
+        val digestBytes = ByteArray(sponge.visibleSize * TypeSize.longSize)
         var pos = 0
-        repeat(visibleSize) {
-            var state = squeeze(it)
-            repeat(8) {
+        repeat(sponge.visibleSize) {
+            var state = sponge.squeeze(it)
+            // Convert little endian Long to big endian bytes
+            (7 downTo 0).forEach {
                 digestBytes[pos++] = (state and 0xff).toByte()
-                state = (state ushr 8)
+                state = state ushr 8
             }
         }
         return digestBytes
