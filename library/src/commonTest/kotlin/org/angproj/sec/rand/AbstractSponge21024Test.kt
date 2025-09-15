198718a66d132aad6437ebd3f07f8a06f69129f1
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2025-09-15 20:16:50 +0200

    Hash test generator

--- library/src/commonTest/kotlin/org/angproj/sec/rand/AbstractSponge21024Test.kt
+++ library/src/commonTest/kotlin/org/angproj/sec/rand/AbstractSponge21024Test.kt
@@ -15,28 +15,18 @@
 package org.angproj.sec.rand
 
 import kotlin.test.Test
-import kotlin.test.assertContentEquals
+import kotlin.test.assertEquals
 
 class AbstractSponge21024Test {
 
+    private val emptyHash = "79be7f41442401bffde34bf348196841555cf3ee86a4cc145a32394fa1977ebde3a1ced98fcf3eabfb6c3d131a25605c031e641ed60ab603a4fe39472142cb1dc6271640c0414e0d94f0febe79ce2c6da2e3cb6df7bd5bd9579bfa94837256dce560f70c2908f1facb4139920c0c95c0de99138448607ec29441a85dee3a1879"
 
     class Hash21024 : Hash<AbstractSponge21024>(object : AbstractSponge21024() {})
 
-
-    val empty = byteArrayOf(
-        110, -55, 28, -3, -82, -57, 44, -68, 82, 31, 66, -71, 15, -39, -95, -21, 106, -23, 16, 116, 67,
-        -23, -114, -111, 59, 35, -38, 12, -82, 47, -17, 14, 85, -42, -34, 23, -33, 7, -111, -43, 12, 86,
-        -19, -102, 17, -27, 94, 28, -94, -14, 49, 39, 88, 71, 106, -36, 82, 85, 4, -56, -87, -111, -87,
-        111, 73, -74, -69, -88, -1, 43, -39, -1, -114, 46, 2, -6, -72, 30, -1, 101, 127, 85, 24, 89, -85,
-        -41, 100, -126, -36, 110, 44, -104, 34, 5, -79, -65, 54, 97, -25, -96, 97, 109, 109, -42, -41,
-        -88, -73, -53, 23, 63, 19, -37, 24, 45, -82, 4, 104, -43, 117, -27, -90, 64, 91, 110, -53, 112, 49, -77
-    )
-
     @Test
-    fun testSqueeze() {
-        val hash = Hash21024()
-        val digest = hash.digest()
-
-        assertContentEquals(digest, empty)
+    fun testEmpty() {
+        val hash = TestGen().runTest(TestGen.NamedTest.TEST_EMPTY, Hash21024())
+        assertEquals(hash, emptyHash)
+        println(hash)
     }
 }
