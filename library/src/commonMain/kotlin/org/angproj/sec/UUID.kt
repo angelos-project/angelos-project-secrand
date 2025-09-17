27c85f352bf138cc8cfc86bf1da027f7f6e79bb7
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2025-09-17 23:24:03 +0200

    Adding UUID

---
+++ library/src/commonMain/kotlin/org/angproj/sec/UUID.kt
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2025 by Kristoffer Paulsson <kristoffer.paulsson@talenten.se>.
+ *
+ * This software is available under the terms of the MIT license. Parts are licensed
+ * under different terms if stated. The legal terms are attached to the LICENSE file
+ * and are made available on:
+ *
+ *      https://opensource.org/licenses/MIT
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Contributors:
+ *      Kristoffer Paulsson - initial implementation
+ */
+package org.angproj.sec
+
+import org.angproj.sec.util.Octet
+
+public abstract class UUID(
+    private val upper: Long = 0,
+    private val lower: Long = 0
+): Octet {
+
+    public constructor() : this(
+        SecureFeed.getNextBits(32).toLong() shl 32 or SecureFeed.getNextBits(32).toLong(),
+        SecureFeed.getNextBits(32).toLong() shl 32 or SecureFeed.getNextBits(32).toLong()
+    )
+
+    public override fun toString(): String {
+        var counter = 0
+        val sb = StringBuilder()
+        counter = printStr<Unit>(upper, sb, counter)
+        printStr<Unit>(lower, sb, counter)
+        return sb.toString()
+    }
+
+    private inline fun<reified E: Any> printStr(src: Long, sb: StringBuilder, counter: Int): Int {
+        var cnt = counter
+        writeLeLong2BeBinary(src, sb, 0, 8) { _, value ->
+            if(counter in hyphens) {
+                cnt += 1
+                sb.append('-')
+            }
+            cnt += 1
+            sb.append(value)
+        }
+        return cnt
+    }
+
+    private companion object {
+        private val hyphens = listOf<Int>(1)
+    }
+}
