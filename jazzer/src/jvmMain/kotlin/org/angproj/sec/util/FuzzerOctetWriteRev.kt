8adc82499a62ab19ce0b26614afa158089619530
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2025-10-14 17:36:25 +0200

    Fuzzing for Octet.readRev and Octet.writeRev OK

---
+++ jazzer/src/jvmMain/kotlin/org/angproj/sec/util/FuzzerOctetWriteRev.kt
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2025 by Kristoffer Paulsson <kristoffer.paulsson@talenten.se>.
+ *
+ * This software is available under the terms of the MIT license. Parts are licensed
+ * under different terms if stated. The legal terms are attached to the LICENSE file
+ * and are made available on:
+ *
+ *      https://opensource.org/licenses/MIT
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Contributors:
+ *      Kristoffer Paulsson - initial implementation
+ */
+package org.angproj.sec.util
+
+import com.code_intelligence.jazzer.Jazzer
+import com.code_intelligence.jazzer.api.FuzzedDataProvider
+import org.angproj.sec.FuzzPrefs
+import kotlin.test.assertEquals
+
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+
+
+public object FuzzerOctetWriteRevKt : FuzzPrefs() {
+
+    @JvmStatic
+    public fun fuzzerTestOneInput(data: FuzzedDataProvider) {
+        val value = data.consumeLong()
+        val array = ByteArray(8)
+
+        Octet.writeRev(value, array, 0, array.size) { index, value ->
+            array[index] = value
+        }
+
+        val buffer = ByteBuffer.wrap(array)
+        if(ByteOrder.nativeOrder() == ByteOrder.BIG_ENDIAN) {
+            buffer.order(ByteOrder.BIG_ENDIAN)
+        }
+        val loaded = buffer.getLong()
+
+        assertEquals(value, loaded)
+    }
+
+    @JvmStatic
+    public fun main(args: Array<String>) {
+        Jazzer.main(arrayOf(
+            "--target_class=${javaClass.name}",
+            "-max_total_time=${maxTotalTime}"
+        ))
+    }
+}
