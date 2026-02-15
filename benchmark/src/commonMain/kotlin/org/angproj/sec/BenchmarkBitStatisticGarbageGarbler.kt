0e269a849674f869cc3c5a60f36e983e5f4b73c1
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2026-02-15 21:31:47 +0100

    Updating support for bit statistical

--- benchmark/src/commonMain/kotlin/org/angproj/sec/BenchmarkBitStatisticGarbageGarbler.kt
+++ benchmark/src/commonMain/kotlin/org/angproj/sec/BenchmarkBitStatisticGarbageGarbler.kt
@@ -21,6 +21,7 @@ import org.angproj.sec.stat.*
 import kotlin.math.abs
 import kotlin.math.absoluteValue
 import kotlin.math.exp
+import kotlin.math.floor
 import kotlin.math.log
 import kotlin.math.log10
 import kotlin.math.log2
@@ -77,44 +78,36 @@ public fun main(args: Array<String> = arrayOf()) {
     }
 
     val garbler = SecureRandom
-    repeat(32) { exp ->
-        val entropy = ByteArray((exp + 1) * 1024) // 16 MiB
+    val entropy = ByteArray(8 * 1024) // 16 MiB
 
-        val max = FloatArray(20)
-        val avg = FloatArray(20)
+    val loops = 10000
+    var totalInside = 0
 
-        val loops = 10000
+    println("logExp: ${log2(entropy.size *4 / 4.0)}")
 
-        println("logExp: ${log2(entropy.size *8 / 4.0)}")
+    repeat(loops) {
+        garbler.readBytes(entropy)
+        val bitStat = bitStatisticOf(entropy)
 
-        repeat(loops) {
-            garbler.readBytes(entropy)
-            val bitStat = bitStatisticOf(entropy)
-
-            val logExp = log2(bitStat.total / 4.0)
-            (0..logExp.toInt()).forEach { idx ->
-                val expectation = 2.0.pow(logExp - idx.toDouble())
-                val difference = expectation - bitStat.runs[idx]
-                //val difference = abs(expectation - bitStat.runs[idx])
-                val deviation = difference.div(expectation).absoluteValue
-                //val tolerance = log(difference +1, idx.toDouble()+1)
-                //println("$idx, $expectation, $difference, $deviation, $tolerance")
-                if (deviation > max[idx]) {
-                    max[idx] = deviation.toFloat()
-                }
-                avg[idx] += deviation.toFloat()
+        val logExp = log2(bitStat.total / 4.0)
+        val length = floor(logExp).toInt()-4
+        var allInside = true
+        (0..length).forEach { idx ->
+            val expectation = 2.0.pow(logExp - idx.toDouble())
+            val deviance = stdDev(idx + 1.0, logExp) * expectation * 5.0 // 3 sigma for 99.7% confidence
+            val tolerance = (expectation - deviance)..(expectation + deviance)
+            val inside: Boolean = tolerance.contains(bitStat.runs[idx].toDouble())
+            if(!inside) {
+                allInside = false
             }
+            //println("$idx, $expectation, ${bitStat.runs[idx]}, tolerance: $tolerance inside: $inside, deviance: $deviance")
         }
-
-        println("Avg runs difference:")
-
-        max.forEachIndexed { idx, value ->
-            println("k=${idx + 1}; avg ${avg[idx] / loops}")
-            //println("k=${idx + 1}; max $value, avg ${avg[idx] / loops}")
-            //println("k=${idx + 1}; max $value, avg ${avg[idx] / loops}, factor ${value / (avg[idx] / loops)}")
+        if(allInside) {
+            totalInside++
         }
-        println()
+        //println("All inside: $allInside")
     }
+    println("Total inside: $totalInside / $loops, percentage: ${totalInside.toDouble() / loops * 100}%")
 
 
     /*var fails = 0
@@ -149,6 +142,6 @@ public fun main(args: Array<String> = arrayOf()) {
 }
 
 // RMSE 0.008.
-public fun f(k: Double, logExp: Double): Double {
+public fun stdDev(k: Double, logExp: Double): Double {
     return 0.031 * exp(0.341 * (k - logExp + 8.488))
 }
