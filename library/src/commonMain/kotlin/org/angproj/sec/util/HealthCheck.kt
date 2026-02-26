8c1ada30486aab70fca86a27098fa693e04ba08b
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2026-02-26 23:12:09 +0100

    Added HealthCheck

---
+++ library/src/commonMain/kotlin/org/angproj/sec/util/HealthCheck.kt
@@ -0,0 +1,113 @@
+/**
+ * Copyright (c) 2026 by Kristoffer Paulsson <kristoffer.paulsson@talenten.se>.
+ *
+ * This software is available under the terms of the MIT license. Parts are licensed
+ * under different terms if stated. The legal terms are attached to the LICENSE file
+ * and are made available on:
+ *
+ *      https://opensource.org/licenses/MIT
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Contributors:
+ *      Kristoffer Paulsson - initial implementation
+ */
+package org.angproj.sec.util
+
+import org.angproj.sec.rand.RandomBits
+import org.angproj.sec.stat.BitStatisticCollector
+import org.angproj.sec.stat.BitStatisticSnapshot
+import org.angproj.sec.stat.securityHealthCheck
+
+
+/**
+ * A utility class for performing health checks on random bit sources by collecting and analyzing bit statistics.
+ *
+ * The `HealthCheck` class extends `BitStatisticCollector` to gather statistics about the bits analyzed, such as
+ * the total number of bits, the count of ones and zeros, hexadecimal distribution, run lengths, and long runs.
+ * It provides methods to analyze bits from various sources, including iterators, byte arrays, and `RandomBits` instances.
+ *
+ * The class also includes companion object functions to perform health checks on `RandomBits` sources, allowing for
+ * both single and double health checks to evaluate the randomness quality of the source.
+ */
+public class HealthCheck : BitStatisticCollector() {
+    private fun consumeLong(value: Long): Unit = consume<Unit>(value, TypeSize.longBits)
+
+    private fun consumeIterator(iter: Iterator<Boolean>) {
+        setup(iter.next().also { collectBit<Unit>(it) })
+        iter.forEach { bit -> collectBit<Unit>(bit) }
+        finish()
+    }
+
+    /**
+     * Analyze bits from an iterator and return a snapshot of the collected statistics.
+     *
+     * @param iter An iterator that provides boolean values representing bits.
+     * @return A snapshot of the collected bit statistics after analyzing the provided bits.
+     */
+    public fun analyze(iter: Iterator<Boolean>): BitStatisticSnapshot {
+        reset()
+        consumeIterator(iter)
+        return snapshot()
+    }
+
+    /**
+     * Analyze bits from a source using a provided function to read octets and return a snapshot of the collected statistics.
+     *
+     * @param src The source from which to read bits.
+     * @param size The number of bytes to read from the source.
+     * @param readOctet A function that takes an index and returns a byte from the source.
+     * @return A snapshot of the collected bit statistics after analyzing the provided bits.
+     */
+    public fun<E> analyze(
+        src: E, size: Int, readOctet: ReadOctet<E, Byte>
+    ): BitStatisticSnapshot = analyze(Octet.bitIterator(0..size, src, readOctet))
+
+    /**
+     * Analyze bits from a ByteArray and return a snapshot of the collected statistics.
+     *
+     * @param src The ByteArray containing the bits to analyze.
+     * @return A snapshot of the collected bit statistics after analyzing the provided bits.
+     */
+    public fun analyze(src: ByteArray): BitStatisticSnapshot = analyze(src, src.size) { idx -> src[idx] }
+
+    /**
+     * Analyze bits from a RandomBits source and return a snapshot of the collected statistics.
+     *
+     * @param randomBits The RandomBits source to analyze.
+     * @return A snapshot of the collected bit statistics after analyzing the provided bits.
+     */
+    public fun analyze(randomBits: RandomBits): BitStatisticSnapshot {
+        reset()
+        val first = RandomBits.nextBitsToLong(randomBits)
+        setup(1L shl (TypeSize.longBits - 1) and first != 0L)
+        consumeLong(first)
+
+        repeat((1024 / TypeSize.longSize) - 1) { _ ->
+            consumeLong(RandomBits.nextBitsToLong(randomBits))
+        }
+        finish()
+        return snapshot()
+    }
+
+    public companion object {
+
+        /**
+         * Perform a health check on the provided RandomBits source by analyzing its bits and evaluating
+         * the security health check.
+         *
+         * @param randomBits The RandomBits source to perform the health check on.
+         * @return `true` if the security health check passes, `false` otherwise.
+         */
+        public fun healthCheck(randomBits: RandomBits): Boolean = HealthCheck().analyze(randomBits).securityHealthCheck()
+
+        /**
+         * Perform a double health check on the provided RandomBits source by conducting two separate health
+         * checks and combining their results.
+         *
+         * @param randomBits The RandomBits source to perform the double health check on.
+         * @return `true` if at least one of the two health checks passes, `false` if both checks fail.
+         */
+        public fun doubleHealthCheck(randomBits: RandomBits): Boolean = healthCheck(randomBits) || healthCheck(randomBits)
+    }
+}
